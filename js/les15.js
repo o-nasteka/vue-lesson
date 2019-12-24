231e7847ac43c10b1f8d7089c2163453a0f4bac4
onasteka <o.nasteka@gmarkg.com>
2019-12-24 16:37:36 +0200

    working with lists

--- js/les15.js
+++ js/les15.js
@@ -2,7 +2,7 @@ new Vue({
 
     el: '#app',
     data: {
-
+        isVisible: true
     },
     computed: {
 
---
+++ js/les16.js
@@ -0,0 +1,22 @@
+new Vue({
+
+    el: '#app',
+    data: {
+        people: [
+            'Vlad',
+            'Max',
+            'Elena',
+            'Marina'
+        ],
+        peoples: [
+            {name: 'Vlad', age: 18},
+            {name: 'Max', age: 21},
+            {name: 'Elena', age: 23},
+        ]
+    },
+    methods: {
+
+    }
+
+});
+
