b64b624057b343a4a660a209d7c2c530a62adf3c
onasteka <o.nasteka@gmarkg.com>
2019-12-24 11:55:55 +0200

    Event Modification

--- js/les8.js
+++ js/les8.js
@@ -2,21 +2,11 @@ new Vue({
 
     el: '#app',
     data: {
-        title: 'Счетчик',
-        counter: 0
+
     },
     methods: {
-        riseCounter: function (num, str, event) {
-            this.counter += num
-            this.title = str
-
-            if (num === 5) {
-                // blue
-                event.target.style.color = 'blue'
-            } else if (num === 10){
-                // red
-                event.target.style.color = 'red'
-            }
+        clickOnGoogle: function (event) {
+            event.preventDefault()
         }
     }
 
