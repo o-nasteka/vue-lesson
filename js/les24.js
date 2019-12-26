afd0d15debbe4326548f6e34c1ef0f658c461199
onasteka <o.nasteka@gmarkg.com>
2019-12-26 17:14:31 +0200

    les 24 vue-cli

---
+++ js/les24.js
@@ -0,0 +1,45 @@
+var vue = new Vue({
+
+    el: '#app',
+    data: {
+        title: 'Hello Vue!'
+    },
+    methods: {
+        doDestroy: function () {
+            this.$destroy()
+        }
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    beforeCreate: function () {
+        console.log('Before Create')
+    },
+    created: function () {
+        console.log('Created')
+    },
+    beforeMount: function () {
+        console.log('beforeMount')
+    },
+    mounted: function () {
+        console.log('mounted')
+    },
+    beforeUpdate: function () {
+        console.log('beforeUpdate')
+    },
+    updated: function () {
+        console.log('Updated')
+    },
+    beforeDestroy: function () {
+        console.log('Before destroy')
+    },
+    destroyed: function () {
+        console.log('Destroyed')
+    }
+});
+
+
+
